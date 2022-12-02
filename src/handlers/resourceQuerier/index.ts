13e7894bcd30be56418b8b2c80e5aabd4ef4dcfb
Kyle <kylereaves@gmail.com>
2022-12-02 10:18:20 -0500

    Moving initialization but the not signer
    back inside of the handler.

--- src/handlers/resourceQuerier/index.ts
+++ src/handlers/resourceQuerier/index.ts
@@ -1,5 +1,5 @@
-import { Context, APIGatewayProxyCallback, APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { Client, QueryResult } from 'pg';
+import { Context, APIGatewayProxyCallback, APIGatewayEvent } from 'aws-lambda';
+import { Client } from 'pg';
 
 const signerOptions = {
     credentials: {
@@ -12,19 +12,19 @@ const signerOptions = {
     username: process.env.USER,
 };
 
-const client = new Client({
-    host: signerOptions.hostname,
-    port: 5432,
-    user: signerOptions.username,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-});
-
 exports.handler = async (
     event: APIGatewayEvent,
     context: Context,
     callback: APIGatewayProxyCallback,
 ): Promise<unknown[]> => {
+    const client = new Client({
+        host: signerOptions.hostname,
+        port: 5432,
+        user: signerOptions.username,
+        database: process.env.DATABASE,
+        password: process.env.PASSWORD,
+    });
+
     console.info('ENVIRONMENT VARIABLES\n' + JSON.stringify(process.env, null, 2));
     console.info('EVENT\n' + JSON.stringify(event, null, 2));
     console.info('CONTEXT\n' + JSON.stringify(context, null, 2));
@@ -39,11 +39,15 @@ exports.handler = async (
 
     const name = event.queryStringParameters?.name;
     const search = event.queryStringParameters?.search;
+    const path = event.path;
 
     const queryParam = name ? name : search;
 
     console.info('name: ' + name);
     console.info('search: ' + search);
+    console.info('path: ' + path);
+
+    // get the resource being querier from the event path
 
     const result = await client.query('SELECT * FROM landlords WHERE name LIKE $1', [`%${queryParam?.toUpperCase()}%`]);
 
